567766e617ac9472ce4f118ef0e843e87861a581
SemaProt <prot.sema2016@gmail.com)>
2017-09-22 00:35:09 +0300

    store worker stats to aws dynamoDB

--- syslog/syslog.js
+++ syslog/syslog.js
@@ -10,50 +10,89 @@ for (var dev in ifaces) {
     ifaces[dev].filter((details) => details.family === 'IPv4' && details.internal === false ? address = details.address: undefined);
 }
 
-console.log(address);
+//console.log(address);
 
+var credentials = {
+    AccessKeyId : "AKIAIVBAO57AHAEMT2WQ",
+    SecretKey   : "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"
+};
+var dynamoDB = require('dynamoDB').DynamoDB(credentials);
 
 var net = require('net'),
     host = address,
     port = 5005,
-    unixsocket = '/tmp/workers-logs.sock';
+    unixsocket = '/tmp/miner-IP37.sock';
 
 var log = function(who, what) {
-  return function() {
-    var args = Array.prototype.slice.call(arguments);
-    console.log('[%s on %s]', who, what, args);
-  };
+    return function() {
+        var args = Array.prototype.slice.call(arguments);
+        console.log('[%s on %s]', who, what, args);
+    };
 };
 
 var echo = function(socket) {
-  /**
-   *  net.Socket (http://nodejs.org/api/net.html#net_class_net_socket)
-   *  events: end, data, end, timeout, drain, error, close
-   *  methods:
-   */
-  socket.on('end', function() {
-    // exec'd when socket other end of connection sends FIN packet
-    console.log('[socket on end]');
-  });
-  var decoder = new StringDecoder('utf8');
-  socket.on('data', function(data) {
-    // data is a Buffer object
-    var textChunk = decoder.write(data);
-    console.log('[socket receive data]', textChunk);
-  });
-  socket.on('end', function() {
-    // emitted when the other end sends a FIN packet
-  });
+    /**
+     *  net.Socket (http://nodejs.org/api/net.html#net_class_net_socket)
+     *  events: end, data, end, timeout, drain, error, close
+     *  methods:
+     */
+    socket.on('end', function() {
+        // exec'd when socket other end of connection sends FIN packet
+        console.log('[socket on end]');
+    });
+    var decoder = new StringDecoder('utf8');
+    socket.on('data', function(data) {
+        // data is a Buffer object
+        var textChunk = decoder.write(data);
+        // console.log('[socket on data]', textChunk);
+        if(textChunk.match('accepted')) {
+            var workerStatsList = textChunk.split(' ');
 
-  socket.on('timeout', log('socket', 'timeout'));
 
-  socket.on('drain', function() {
-    // emitted when the write buffer becomes empty
-    console.log('[socket on drain]');
-  });
-  socket.on('error', log('socket', 'error'));
-  socket.on('close', log('socket', 'close'));
-  socket.pipe(socket);
+            // console.log(
+            //     "workerID", workerStatsList[3].replace(/\[.*\].*/, '').replace(/worker@/, ''),
+            //     "\ndate", workerStatsList[4].replace(/\[/, ''),
+            //     "\ntime", workerStatsList[5].replace(/\]/, ''),
+            //     "\naccepted", workerStatsList[7] + ' ' + workerStatsList[8].replace(/,/, ''),
+            //     "\nHs", workerStatsList[9],
+            //     "\ndiff", workerStatsList[13]
+            // )
+
+            dynamoDB.putItem(
+                {"TableName":"Workers",
+                    "Item":{
+                        "workerID" : {"S": workerStatsList[3].replace(/\[.*\].*/, '').replace(/worker@/, '')},
+                        "date" : {"S": workerStatsList[4].replace(/\[/, '')},
+                        "time" : {"S": workerStatsList[5].replace(/\]/, '')},
+                        "accepted"  : {"S": workerStatsList[7] + ' ' + workerStatsList[8].replace(/,/, '')},
+                        "Hs": {"N": workerStatsList[9]},
+                        "diff": {"N": workerStatsList[13]}
+                    }
+                }
+                , function(response,result) {
+                    response.on('data', function(chunk){
+                        //console.log(""+chunk);
+                    });
+                    result.on('ready', function(data){
+                        //console.log("Error:" + data.error);
+                        //console.log("ConsumedCapacityUnits:" + data.ConsumedCapacityUnits);
+                    });
+                });
+        }
+    });
+    socket.on('end', function() {
+        // emitted when the other end sends a FIN packet
+    });
+
+    socket.on('timeout', log('socket', 'timeout'));
+
+    socket.on('drain', function() {
+        // emitted when the write buffer becomes empty
+        console.log('[socket on drain]');
+    });
+    socket.on('error', log('socket', 'error'));
+    socket.on('close', log('socket', 'close'));
+    socket.pipe(socket);
 };
 
 /**
@@ -65,22 +104,23 @@ var server = net.createServer(echo);
 server.listen(port, host); // port or unix socket, cannot listen on both with one server
 
 server.on('listening', function() {
-  var ad = server.address();
-  if (typeof ad === 'string') {
-    console.log('[server on listening] %s', ad);
-  } else {
-    console.log('[server on listening] %s:%s using %s', ad.address, ad.port, ad.family);
-  }
+    var ad = server.address();
+    if (typeof ad === 'string') {
+        console.log('[server on listening] %s', ad);
+    } else {
+        console.log('[server on listening] %s:%s using %s', ad.address, ad.port, ad.family);
+    }
 });
 
 server.on('connection', function(socket) {
-  server.getConnections(function(err, count) {
-    console.log('%d open connections!', count);
-  });
+    server.getConnections(function(err, count) {
+        console.log('%d open connections!', count);
+    });
 });
 
 server.on('close', function() { console.log('[server on close]'); });
+
 server.on('err', function(err) {
-  console.log(err);
-  server.close(function() { console.log("shutting down the server!"); });
+    console.log(err);
+    server.close(function() { console.log("shutting down the server!"); });
 });
